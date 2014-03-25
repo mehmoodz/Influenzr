eef4349e6501a0fa927a94c5a099bef899906525
mkhan <mehmood.khan@digitas.com>
2014-03-24 21:00:53 -0400

    Tweet Count Bolt Added

--- src/main/java/com/startup/trends/process/TweetCounterBolt.java
+++ src/main/java/com/startup/trends/process/TweetCounterBolt.java
@@ -2,16 +2,21 @@ package com.startup.trends.process;
 
 import java.util.Map;
 
+import twitter4j.Status;
+
 import backtype.storm.task.OutputCollector;
 import backtype.storm.task.TopologyContext;
 import backtype.storm.topology.OutputFieldsDeclarer;
 import backtype.storm.topology.base.BaseRichBolt;
+import backtype.storm.tuple.Fields;
 import backtype.storm.tuple.Tuple;
+import backtype.storm.tuple.Values;
 
-public class StemmingBolt extends BaseRichBolt {
+public class TweetCounterBolt extends BaseRichBolt {
 
 	private static final long serialVersionUID = -4922677756054234041L;
 	private OutputCollector outputCollector;
+	private long tweetCount=0;
 
 	public void prepare(Map stormConf, TopologyContext context,
 			OutputCollector collector) {
@@ -21,10 +26,13 @@ public class StemmingBolt extends BaseRichBolt {
 
 	public void execute(Tuple input) {
 		// TODO Auto-generated method stub
+		final Status status = (Status) input.getValueByField("tweet");
+		this.outputCollector.emit(new Values(++tweetCount, status));
 	}
 
 	public void declareOutputFields(OutputFieldsDeclarer declarer) {
 		// TODO Auto-generated method stub
+		declarer.declare(new Fields("count", "tweet"));
 	}
 	 
 }
